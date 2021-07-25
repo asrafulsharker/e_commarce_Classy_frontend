4b1214ed5ca6ff3ba3048b18bd3e10fe7c8f3589
Md Asraful Sharker  Nirob <64266026+asrafulsharker@users.noreply.github.com>
2021-07-25 20:55:31 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import {Container} from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+import HomeScreen  from './screens/HomeScreen';
 function App() {
   return (
     <div >
       <Header/>
       <main className="py-3">
           <Container>
-          <h1>Welcome to Classy Style</h1>
+          <HomeScreen/>
           </Container>
       </main>
       <Footer/>
