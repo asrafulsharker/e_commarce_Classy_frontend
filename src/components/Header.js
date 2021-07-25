a2e74075559f7af49c966bb8928c4bdd18f4a320
Md Asraful Sharker  Nirob <64266026+asrafulsharker@users.noreply.github.com>
2021-07-26 00:06:11 +0600

    add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Navbar, Nav, Container, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 function Header() {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                <Navbar.Brand href="/">Classy Style</Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand >Classy Style</Navbar.Brand>
+                    </LinkContainer>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                     className="mr-auto"
                     >
-                    <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-                    <Nav.Link href="/login"><i className="fas fa-user"></i>Login</Nav.Link>
+                    <LinkContainer to="/cart">
+                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/login">
+                        <Nav.Link ><i className="fas fa-user"></i>Login</Nav.Link>
+                    </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
                 </Container>
