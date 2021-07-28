c964960e3d2963e6aba561811c21ac42eb3a37d3
Md Asraful Sharker  Nirob <64266026+asrafulsharker@users.noreply.github.com>
2021-07-28 18:02:18 +0600

    add

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import {Row, Col} from 'react-bootstrap';
-import Product from '../components/Product'
+import Product from '../components/Product';
+import axios from 'axios';
+
 import products from '../products';
 function HomeScreen() {
+    const [products, setProducts] = useState([])
+
+
+    useEffect(()=>{
+        async function fetchProducts(){
+            const { data } = await axios.get('/api/products/')
+            setProducts(data)
+        }
+        fetchProducts()
+    },[])
+
     return (
         <div>
             <h1>Latest Products</h1>
