52c410d48f7565b81de5b3131c4660dc3b9b9c13
Md Asraful Sharker  Nirob <64266026+asrafulsharker@users.noreply.github.com>
2021-09-08 13:55:16 +0600

    add

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -9,7 +9,7 @@ export const listProducts =()=> async(dispatch)=>{
     try{
         dispatch({type: PRODUCT_LIST_REQUEST})
         const {data} = await axios.get('api/products/')
-    
+   
         dispatch({
             type:PRODUCT_LIST_SUCCESS,
             payload: data
